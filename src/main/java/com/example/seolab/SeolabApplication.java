3c4895c670d1a1ef79eceec01958b62f393e7cec
mingwan21 <ibini12@gmail.com>
2025-07-05 02:27:28 +0900

    .properties change to .yml

---
+++ src/main/java/com/example/seolab/SeolabApplication.java
@@ -0,0 +1,13 @@
+package com.example.seolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeolabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seolab/SeolabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
